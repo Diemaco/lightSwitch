1ad1c6bcfb1fad3796e998d59306e11a1bf72a89
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 10:18:56 +0100

    Updated script.js

--- html javascript/script.js
+++ html javascript/script.js
@@ -1,10 +1,25 @@
 var btn = document.createElement('button');
 btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.innerHTML = 'Switch light on';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
+document.body.style.backgroundColor = 'black';
 
+function switchBG() {
+    var isOn = document.body.style.backgroundColor === 'yellow';
 
+    if (isOn) {
+        document.body.style.backgroundColor = 'black';
+        console.log('light is off');
+        btn.innerHTML = 'Switch light on';
+    } else {
+        document.body.style.backgroundColor = 'yellow';
+        console.log('light is on');
+        btn.innerHTML = 'Switch light off';
+    }
+}
+
+btn.onclick = switchBG;
 
 // schijf hier tussen je code
