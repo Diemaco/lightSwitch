82b11419fabfe60692c147c7bff4639adbea154a
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 10:00:54 +0100

    Updated lightswitch.py

--- lightswitch.py
+++ lightswitch.py
@@ -1,11 +1,21 @@
 import tkinter as tk
+
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+button = tk.Button(text='Switch light on', bg="white", fg="black")
+button.pack(pady=20, padx=20)
 
 # schijf hier tussen je code
 
+button.configure(command=lambda: switchBG())
+
+
+def switchBG():
+    isOn = window['bg'] == 'yellow'
+
+    button.configure(text='Switch light ' + ('off' if isOn else 'on'))
+    window.configure(bg='black' if isOn else 'yellow')
+    print('light is ' + ('off' if isOn else 'on'))
 
 
 # schijf hier tussen je code
